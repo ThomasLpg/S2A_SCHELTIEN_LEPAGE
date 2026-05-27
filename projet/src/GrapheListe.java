86c2e3ad59421b8b8833870ded11bc0e4fc9ed90
LEPAGE Thomas <thomaslegithub@gmail.com>
2026-05-27 13:29:35 +0200

    Méthode ajouterArc complétée

--- projet/src/GrapheListe.java
+++ projet/src/GrapheListe.java
@@ -1,8 +1,9 @@
+import javax.crypto.spec.DESedeKeySpec;
+import java.util.AbstractList;
 import java.util.ArrayList;
 
-public class GrapheListe implements Graphe{
+public class GrapheListe implements Graphe {
     ArrayList<String> nodeList;
-
     ArrayList<Arcs> adjNodeList;
 
     @Override
@@ -11,13 +12,28 @@ public class GrapheListe implements Graphe{
     }
 
     @Override
-    public ArrayList<Arcs>getAdjNodeList() {
+    public ArrayList<Arcs> getAdjNodeList() {
         return adjNodeList;
     }
 
-    public void ajouterArc(String source, String dest, int poids){
-        
+    public void addNode(String source, String dest, int poids){
+        int indexSource = this.nodeList.indexOf(source);
+
+        Arcs arcs = new Arcs();
+        arcs.ajouterArc(new Arc(dest, poids));
+
+        this.adjNodeList.add(indexSource, arcs);
     }
 
-
+    public void ajouterArc(String source, String dest, int poids) {
+        //Cas où le noeud source existe dans la liste de noeud et que dans la liste d'adjacence, l'index est deja
+        // raccordé au noeud source
+        if (this.nodeList.contains(source) && this.nodeList.contains(dest)) {
+            addNode(source, dest, poids);
+        }else{
+            this.nodeList.add(source);
+            addNode(source, dest, poids);
+        }
+    }
 }
+
